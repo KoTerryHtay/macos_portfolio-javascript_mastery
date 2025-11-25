6f39ff8338677d131152153ca60faa90acacbe63
KoTerryHtay <luaye1990@gmail.com>
2025-11-25 19:19:50 +0630

    Implement Safari Browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 import { Dock, Navbar, Welcome } from "@/components";
-import { Terminal } from "@/windows";
+import { Safari, Terminal } from "@/windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -13,6 +13,7 @@ export default function App() {
       <Dock />
 
       <Terminal />
+      <Safari />
     </main>
   );
 }
